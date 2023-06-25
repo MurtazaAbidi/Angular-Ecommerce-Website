824a5abfb54cc0d11b5d1112f6f29985989ece39
Murtaza Abidi <murtaza2476175@gmail.com>
2023-06-25 12:38:43 +0500

    SignUp form

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class SellerAuthComponent {
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  signUp(data: object): void {
+    console.warn (data)
+  }
+
 }
